6128eb91b3fa8c42e8abd414d084d2e90704e15e
Mohamed Amarcha <simoamarcha@icloud.com>
2023-02-24 04:09:25 +0100

    config 3

---
+++ src/main/java/com/tripleD/app/configuration/WebConfig.java
@@ -0,0 +1,20 @@
+package com.tripleD.app.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .exposedHeaders("Authorization")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
